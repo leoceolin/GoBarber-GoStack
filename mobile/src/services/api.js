b887012225c2b799c6deb9371541f11fc832ec18
Leonel Ceolin Farias <leonell_ceolin@hotmail.com>
2019-11-20 13:37:42 -0400

    imagens

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://192.168.15.5:3333' })
+const api = axios.create({ baseURL: 'http://192.168.15.4:3333' })
 
 export default api
